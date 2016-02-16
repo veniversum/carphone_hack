2bb73a49b5f4c16f78bbbcbf7d8a40622a6a9acc
jakow <qbol1234@hotmail.com>
2016-02-16 16:27:52 +0000

    even more frontend boilerplate

--- frontend/dev/js/main.js
+++ frontend/dev/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-
+    $('#hello').append('Hello world!');
+    $('#hello').append('Hello world!');
 
 });
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 var bowerSources = [
     "./bower_components/jquery/dist/jquery.js",
@@ -18,17 +19,19 @@ gulp.task("js", function() {
     console.log(srcs);
     gulp.src(srcs)
         .pipe(concat("app.js"))
-        .pipe(gulp.dest('./build/js/'));
+        .pipe(gulp.dest('./build/js/'))
+        .pipe(connect.reload());
 });
 
 gulp.task("copy", function() {
     gulp.src('./dev/html/**/*.html')
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest('./build'))
+        .pipe(connect.reload());
 });
 
-gulp.task('build', ['js', 'copy']);
+gulp.task('build', ['js', 'copy', 'sass']);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['build' ],function() {
     gulp.watch(jsSources, ['js']);
     gulp.watch('./dev/html/**/*.html', ['copy']);
     gulp.watch('./dev/sass/**/*.scss', ['sass']);
@@ -38,5 +41,13 @@ gulp.task('sass', function() {
     return gulp.src('./dev/sass/stylesheet.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(concat('stylesheet.css'))
-        .pipe(gulp.dest('./build/css'));
+        .pipe(gulp.dest('./build/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('serve', ['watch'], function() {
+    connect.server({
+        root: 'build',
+        livereload: true
+    });
 });
