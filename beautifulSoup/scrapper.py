212d396dff63f512ff8cb0369b781966894eb877
goweiting <gwt.543@gmail.com>
2016-02-16 21:19:25 +0000

    addded listOfDetails

--- beautifulSoup/scrapper.py
+++ beautifulSoup/scrapper.py
@@ -3,19 +3,28 @@ import urllib2
 import re
 import os
 
-# to be included:
+# get all the links for PC LAPTOP CATEGORY
 linkLaptop = []
+f = open('pclinks.txt','r')
+for line in f:
+    line = line[:-1] #does it really matters?
+    linkLaptop.append(line)
 
+listOfdetails = []
+# scrab each  links
 for link in linkLaptop:
     content = urllib2.urlopen(link).read()
     soup = BeautifulSoup(content,"lxml")
     simpleTable = soup.select(".simpleTable > tbody > tr")
-    # print simpleTable
 
     details = {}
     for tr in simpleTable:
         key = tr.th.text
-        cups = list(tr.td.text)
+        cups = tr.td.text
         details[key] = cups
 
-    
+    listOfdetails.append(details)
+#
+# print "hello"
+# for i in listOfdetails:
+#     print i
