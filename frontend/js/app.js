ab3e908c809721be8fb7e2cbe7b189d77ddb94a9
jakow <qbol1234@hotmail.com>
2016-02-19 01:35:19 +0000

    push for frontend testing

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -28,14 +28,21 @@ $(document).ready(function(){
     $('#gaming').change(function() {
         var elem = this;
         if(this.checked) {
-            $('#choose-game-tier').slideDown();
-            $('.background-image').each(function(index, elem) {
-                adjustImgSize(elem);
+            $('#choose-game-tier').slideDown(function() {
+                $('.background-image').each(function (index, elem) {
+                    adjustImgSize(elem);
+                    //$(elem).fadeIn();
+
+                });
+
+
             });
             $('.background-image').fadeIn();
         }
         else {
-            $('.background-image').fadeOut();
+            $('.background-image').fadeOut(function() {
+
+            });
             $('#choose-game-tier').slideUp();
 
         }
@@ -47,10 +54,15 @@ $(document).ready(function(){
     $('.next-page').click(function() {
         //validate form, then
         //$('#page'+currentPage).;
-        $('#page'+currentPage).hide("slide", {direction: "left"}, 350, function() {
-            currentPage = currentPage + 1;
-            $('#page' + currentPage).show("slide", {direction: "right"}, 350);
-        });
+        if(currentPage == 1 ? validate1() : validate2()) {
+            $('#page' + currentPage).hide("slide", {direction: "left"}, 350, function () {
+                currentPage = currentPage + 1;
+                $('#page' + currentPage).show("slide", {direction: "right"}, 350);
+            });
+        }
+        else {
+            //feedback pls
+        }
     });
 
     $('.prev-page').click(function() {
@@ -59,7 +71,13 @@ $(document).ready(function(){
         currentPage = currentPage-1;
         $('#page'+currentPage).show("slide", {direction: "left"}, 350);
     })
+
+
 });
+
+    $('#portability-slider').change(function() {
+        console.log(this.value);
+    })
 });
 
 
@@ -68,4 +86,35 @@ function adjustImgSize(elem) {
     var height = parent.height();
     var width = parent.width();
     $(elem).css({'width':width, 'height':height})
+}
+
+function validate1() {
+    //check if any of checkboxes checked
+    var checkboxes = $('#main-purpose').children("[type='checkbox']");
+    var selected = false;
+    checkboxes.each(function(index, elem) {
+        selected = selected || elem.checked;
+    });
+    if (!selected) {
+        return false;
+    }
+
+    var gaming = $('#gaming');
+    if(gaming.checked) {
+        var gameTiers = $('#choose-game-tier').children("[id$='-end']");
+        var checked = false;
+        gameTiers.each(function(index, elem) {
+            checked = checked || elem.checked;
+        });
+        if (!checked) return false;
+    }
+
+    var media = $('#media');
+
+
+    return true;
+}
+
+function validate2() {
+    return true;
 }
